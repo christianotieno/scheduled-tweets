31fc61ceb5aab88d9f6c4f56089cc2beed4f7cc2
christianotieno <oduorchristian@gmail.com>
2021-03-10 00:23:26 +0300

    Add bootstrap css and javascript

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Rails.application.routes.draw do
-  # GET /about
-  get '/about', to: "about#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  get '/about', to: "about#index"
+  get '/', to: "main#index"
 end
