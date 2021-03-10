1dc2dd0ea9133976c9f6c529d65f849d97bc3e8c
christianotieno <oduorchristian@gmail.com>
2021-03-11 02:23:36 +0300

    Add ahndling signup errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,19 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'Successfully created account'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confitmation)
+  end
 end
