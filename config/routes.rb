1b38908be7009c887c2baee6be050ae376b5ca8c
christianotieno <oduorchristian@gmail.com>
2021-03-11 04:52:35 +0300

    Add update password for logged in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   get '/about', to: "about#index"
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
