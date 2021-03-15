cbe8208c4b1cf738bf7fe1cd73083674a6490f69
christianotieno <oduorchristian@gmail.com>
2021-03-15 22:32:43 +0300

    Add setting records with before actions

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -1,5 +1,6 @@
 class TwitterAccountsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_twitter_account, only: [:destroy]
   def index
     @twitter_accounts = Current.user.twitter_accounts
   end
@@ -8,5 +9,11 @@ class TwitterAccountsController < ApplicationController
     @twitter_account = Current.user.twitter_accounts.find(params[:id])
     @twitter_account.destroy
     redirect_to twitter_accounts_path, notice: "Successfully disconnected: @#{@twitter_account.username}"
+  end
+
+  private
+
+  def set_twitter_account
+    @twitter_account = Current.user.twitter_accounts.find(params[:id])
   end
 end
